dc5e1cfd05225c7081c451796d3016d7f91868cd
nbitzz <77242831+nbitzz@users.noreply.github.com>
2022-05-22 16:10:57 -0700

    Install robloxts

--- bot/optipost.ts
+++ bot/optipost.ts
@@ -2,6 +2,45 @@ import express from "express"
 import bodyparser from "body-parser"
 import { BaseEvent,EventSignal } from "./events"
 
+interface JSONCompliantArray {
+    [key:number]:string|number|boolean|JSONCompliantObject|JSONCompliantArray|null
+}
+
+interface JSONCompliantObject {
+    [key:string]:string|number|boolean|JSONCompliantObject|JSONCompliantArray|null
+}
+
+interface BasicReply {
+    type:string
+    data:JSONCompliantObject
+}
+
+export class OptipostRequest {
+    readonly request:express.Request
+    readonly response:express.Response
+    private readonly Autostop:NodeJS.Timeout
+    readonly KillTimestamp:number
+    readonly Dead:boolean=false
+
+    constructor(req:express.Request,res:express.Response) {
+        this.request = req
+        this.response = res
+        this.KillTimestamp = Date.now()+15000
+        this.Autostop = setTimeout(() => {
+            
+        },15000)
+    }
+    Kill() {
+        this.Reply({
+            type:"Kill",
+            data:{}
+        })
+    }
+    Reply(data:BasicReply) {
+        this.response.send(JSON.stringify(data))
+    }
+}
+
 export class Optipost {
     readonly app:express.Application
     readonly port:number
--- roblox/optipost.lua
+++
@@ -1,2 +0,0 @@
-local http = game:GetService("HttpService")
-
