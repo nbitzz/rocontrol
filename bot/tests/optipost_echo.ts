f1e67b55c9d3a95cc384e836396366f6ce91d00a
nbitzz <77242831+nbitzz@users.noreply.github.com>
2022-05-22 21:33:28 -0700

    Test

--- bot/tests/optipost_echo.ts
+++ bot/tests/optipost_echo.ts
@@ -3,7 +3,9 @@ import { Optipost, OptipostSession } from "../optipost"
 let opti = new Optipost()
 
 opti.connection.then((connection:OptipostSession) => {
+    console.log("Connection")
     connection.message.then((data) => {
+        console.log(data)
         connection.Send(data)
     })
 })
--- server_out/tests/optipost_echo.js
+++ server_out/tests/optipost_echo.js
@@ -3,7 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const optipost_1 = require("../optipost");
 let opti = new optipost_1.Optipost();
 opti.connection.then((connection) => {
+    console.log("Connection");
     connection.message.then((data) => {
+        console.log(data);
         connection.Send(data);
     });
 });
